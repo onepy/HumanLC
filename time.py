4df3d5fc111ad7dddbf9583288887d0cca64460f
ponyoung <1491236951@qq.com>
2024-12-15 00:44:28 +0800

    Update time.py

--- time.py
+++ time.py
@@ -5,14 +5,14 @@ from datetime import datetime
 from common.log import logger
 import pytz
 
-@plugins.register(name="Time", desc="告诉gpt当前时间", version="0.1", author="pon", desire_priority=990)
+@plugins.register(name="Time", desc="告诉GPT当前时间, 并插入到system角色", version="0.1", author="pon", desire_priority=990)
 class Time(Plugin):
     def __init__(self):
         super().__init__()
         self.handlers[Event.ON_HANDLE_CONTEXT] = self.on_handle_context
         logger.info("[Time] inited")
 
-    def get_current_time_with_timezone(self,timezone_name):
+    def get_current_time_with_timezone(self, timezone_name):
         timezone = pytz.timezone(timezone_name)
         now = datetime.now(timezone)
         return now.strftime("%Y-%m-%d %H:%M:%S (%A)")
@@ -24,13 +24,15 @@ class Time(Plugin):
         # 获取当前时间，使用你的目标时区
         time_str = self.get_current_time_with_timezone("Asia/Shanghai")
 
-        # 将时间信息添加到消息内容中
-        original_content = e_context['context'].content
-        modified_content = f"{original_content} [{time_str}]"
-        e_context['context'].content = modified_content
+        # 插入到 system 角色的内容中
+        system_message = f"当前时间是 {time_str}"
 
-        logger.debug(f"[Time] Modified content to: {modified_content}")
+        # 假设 context['msg'] 是一个包含 role 和 content 的字典结构
+        if 'msg' in e_context['context'] and isinstance(e_context['context']['msg'], dict):
+            e_context['context']['msg'].setdefault('system', []).append({"role": "system", "content": system_message})
+
+        logger.debug(f"[Time] Added system message: {system_message}")
         e_context.action = EventAction.CONTINUE
 
     def get_help_text(self, **kwargs):
-         return "告诉GPT当前时间（上海时区）"
+        return "告诉GPT当前时间（上海时区），并将时间信息插入到system角色中"
