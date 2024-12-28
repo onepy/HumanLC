9a565efb952d470b65aaac75bf5048691e8cc7b0
ponyoung <1491236951@qq.com>
2024-12-28 11:03:35 +0800

    Update humanlc.py

--- humanlc.py
+++ humanlc.py
@@ -6,7 +6,7 @@ from bridge.reply import Reply, ReplyType
 import time
 import threading
 
-@plugins.register(name="humanlc", desc="A simple plugin that delays and accumulates private messages", version="0.1", author="Pon")
+@plugins.register(name="humanlc", desc="A simple plugin that delays and accumulates private messages", version="0.2", author="Pon")
 class humanlc(Plugin):
     def __init__(self):
         super().__init__()
@@ -37,7 +37,14 @@ class humanlc(Plugin):
             if not event.is_set():  # 如果没有设置超时
                 threading.Thread(target=self.wait_timeout, args=(user_id, current_time, e_context), daemon=True).start()  # 创建新的线程进行超时等待
                 logger.debug(f"[humanlc] userId:{user_id} accumulate_messages, count:{len(message_list)}, intercept message. content: {e_context['context'].content}")
-                return  # 不设置 e_context.action， 保持默认的 EventAction.CONTINUE
+                e_context.action = EventAction.BREAK_PASS # 拦截消息，不传递给后续流程
+                return
+            else:
+                # 超时线程处理过了，消息传递给下一个流程
+                with self.lock:
+                    if user_id in self.accumulated_messages:
+                      self.accumulated_messages[user_id][2].clear() # 清除超时标记
+                return
         else:
             # 累积够5条消息
             with self.lock:
@@ -51,11 +58,14 @@ class humanlc(Plugin):
         event = self.accumulated_messages[user_id][2]
         if event.wait(10): # 设置超时时间
             return # 被其他消息线程设置超时了
-
+        
         with self.lock:
+            if user_id not in self.accumulated_messages:
+                return
             message_list, last_message_time, _ = self.accumulated_messages[user_id]
             if last_message_time == current_time and len(message_list) > 0: # 10秒内没有收到新消息,并且有消息需要处理
                 e_context["context"].content = " ".join(message_list)
                 self.accumulated_messages[user_id] = [[], None, threading.Event()] # 清空消息列表
-                event.set() # 设置超时
                 logger.debug(f"[humanlc] userId:{user_id} accumulate_messages timeout, pass on to the next level. content: {e_context['context'].content}")
+                event.set()
+            
